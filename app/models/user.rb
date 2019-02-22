5b2f3b2e0507d00cfeb203e361363af5961f3abc
bribrah <cheesemas46@gmail.com>
2019-02-21 16:39:48 -0800

    Finished suer edit, index, delete, and update

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 50}  
     validates :email, presence: :true, length: {maximum:255}, format: {with:VALID_EMAIL_REGEX}, 
     uniqueness: { case_sensitive: false}
-    validates :password, presence: :true,  length: {minimum:6 }
     has_secure_password
+    validates :password, presence: :true,  length: {minimum:6 }, allow_nil: true
     
     #returns hash of given string
     def User.digest(string)
