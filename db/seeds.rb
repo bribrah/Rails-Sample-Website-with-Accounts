37f0855ef95657afc6658b900daaad0eb06cd4b1
bribrah <cheesemas46@gmail.com>
2019-02-28 15:42:29 -0800

    admin changes:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "Example User",email: "example@example.com", password: "foobar", password_confirmation: "foobar", admin: true)
+User.create!(name: "Example User",email: "example@example.com", password: "foobar", password_confirmation: "foobar")
 
 99.times do |n|
     name = Faker::Name.name
